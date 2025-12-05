c9417a2a5d6f35066c556740a9ca10c92d3bc6ad
asdfwyay <unconfigured@null.spigotmc.org>
2025-12-05 02:04:36 -0500

    refactor to have all locations in a single table

--- worlds/buckshot/Locations.py
+++ worlds/buckshot/Locations.py
@@ -1,30 +1,41 @@
+from dataclasses import dataclass
+from enum import IntEnum
 from BaseClasses import Location
 
 class BuckshotRouletteLocation(Location):
     game = "Buckshot Roulette"
 
-base_game_locations: dict[str, int] = {
-    "Win Round 1": 1,
-    "Win Round 2": 2,
-    "Win Round 3": 3
-}
+class LocationType(IntEnum):
+    BASE_GAME = 0
+    ACHIEVEMENT = 1
+    DOUBLE_OR_NOTHING = 2
+    SHOTSANITY = 3
+    MULTIPLAYER = 4 # UNUSED
 
-achievement_locations: dict[str, int] = {
-    "70K": 4,
-    "Coin Flip": 5,
-    "Chasing Losses": 6,
-    "Bronze Gates": 7,
-    "High Rollers": 8,
-    "Name Taken": 9,
-    "Nope!": 10,
-    "140K": 11,
-    "Overdose": 12,
-    "Soak It In": 13,
-    "Digita, Orava and Koni": 14,
-    "Why?": 15,
-    "Going Out With Style!": 16,
-    "1000K": 17,
-    "Full House": 18,
-    "Know When To Quit": 19
-}
+@dataclass
+class LocationData:
+    id: int
+    location_type: LocationType
 
+location_table: dict[str, LocationData] = {
+    "Win Round 1": LocationData(1, LocationType.BASE_GAME),
+    "Win Round 2": LocationData(2, LocationType.BASE_GAME),
+    "Win Round 3": LocationData(3, LocationType.BASE_GAME),
+
+    "70K": LocationData(4, LocationType.ACHIEVEMENT),
+    "Coin Flip": LocationData(5, LocationType.ACHIEVEMENT),
+    "Chasing Losses": LocationData(6, LocationType.ACHIEVEMENT),
+    "Bronze Gates": LocationData(7, LocationType.ACHIEVEMENT),
+    "High Rollers": LocationData(8, LocationType.ACHIEVEMENT),
+    "Name Taken": LocationData(9, LocationType.ACHIEVEMENT),
+    "Nope!": LocationData(10, LocationType.ACHIEVEMENT),
+    "140K": LocationData(11, LocationType.ACHIEVEMENT),
+    "Overdose": LocationData(12, LocationType.ACHIEVEMENT),
+    "Soak It In": LocationData(13, LocationType.ACHIEVEMENT),
+    "Digita, Orava and Koni": LocationData(14, LocationType.ACHIEVEMENT),
+    "Why?": LocationData(15, LocationType.ACHIEVEMENT),
+    "Going Out With Style!": LocationData(16, LocationType.ACHIEVEMENT),
+    "1000K": LocationData(17, LocationType.ACHIEVEMENT),
+    "Know When To Quit": LocationData(18, LocationType.ACHIEVEMENT),
+    "Full House": LocationData(19, LocationType.ACHIEVEMENT),
+}
