85085ea4924ffb5421718ab2316bd9d76c2925d7
asdfwyay <unconfigured@null.spigotmc.org>
2025-12-08 17:02:59 -0500

    Added rules

---
+++ worlds/buckshot/Rules.py
@@ -0,0 +1,26 @@
+from typing import Callable
+from BaseClasses import CollectionState
+from .Enums import *
+from .Items import item_table
+
+def consumable_rule(world, count: int, exclude_don=False) -> Callable[[CollectionState], bool]:
+    consumables: list[str] = [
+        item_name
+        for item_name, item_data in item_table.items()
+        if item_data.flags & (I_CONSUMABLE | I_DOUBLE_OR_NOTHING if exclude_don else I_CONSUMABLE)
+    ]
+    return lambda state: state.has_from_list(consumables, world.player, count)
+
+def specific_consumables_rule(world, consumables: list[str]) -> Callable[[CollectionState], bool]:
+    return lambda state: state.has_all(consumables, world.player)
+
+def full_house_rule(world) -> Callable[[CollectionState], bool]:
+    consumables: list[str] = [
+        item_name
+        for item_name, item_data in item_table.items()
+        if (item_data.flags & I_CONSUMABLE) and item_name != "Adrenaline"
+    ]
+    return lambda state: state.has("Adrenaline", world.player) and state.has_from_list(consumables, world.player, 7)
+
+def don_access_rule(world, items: list[str]) -> Callable[[CollectionState], bool]:
+    return lambda state: state.has_all(items, world.player)
